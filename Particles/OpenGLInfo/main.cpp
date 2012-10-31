a70f684d4c6647fa8a2ba433fa2e006cfd20bbf8
Ben Force <benforce@gmail.com>
2012-10-30 21:17:32 -0500

    OpenGL Info Compiles

--- Particles/OpenGLInfo/main.cpp
+++ Particles/OpenGLInfo/main.cpp
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <GL/glew.h>
+#include <GL/freeglut.h>
 
 int main(int argc, char* argv[])
 {
+	glutInit(&argc, argv);
+
 	fprintf(
 			stdout,
 			"INFO: OpenGL Version: %s\n",
